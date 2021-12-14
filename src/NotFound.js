af93b87cbc2873d912dc3283d968714f0d10ff30
Caipora Games <73959333+CaiporaGames@users.noreply.github.com>
2021-12-14 20:52:43 -0300

    Submit complete project
    This project is complete using react

---
+++ src/NotFound.js
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom'
+
+const NotFound = () =>
+{
+    return(
+        <div className = 'not-found'>
+            <h2>Sorry</h2>
+            <p>
+                Page not found
+            </p>
+            <Link to="/" >Go to home</Link>
+        </div>
+    )
+}
+
+export default NotFound
